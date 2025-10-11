a4ba0a655a73f63850c9b06813692647b31d20c7
LooseJuice1075 <hobo1075@gmail.com>
2025-10-11 11:02:49 -0400

    Premake5 Setup

---
+++ premake5.lua
@@ -0,0 +1,32 @@
+workspace "StudentTracker"
+	architecture "x64"
+	startproject "StudentTracker"
+	
+	configurations
+	{
+		"Debug",
+		"Release"
+	}
+	
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "StudentTracker"
+	location "StudentTracker"
+	kind "ConsoleApp"
+	language "C#"
+	
+	-- targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	-- objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+	
+	files
+	{
+		"%{prj.name}/src/**.cs"
+	}
+	
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
