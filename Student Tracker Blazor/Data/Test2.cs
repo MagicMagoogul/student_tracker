18fa70c34cead2ab71bcb41b89230bfa197b29d1
Aurora Liles <LILESA@ETSU.EDU>
2025-10-27 14:03:53 -0400

    FUCK

--- Student Tracker Blazor/Data/Test2.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Student_Tracker_Blazor.Data
-{
-    public class Test2
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string TestTwo { get; set; }
-    }
-}
