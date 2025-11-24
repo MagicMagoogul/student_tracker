ece7c6e65f3cbe3d588e89e7370b1eeb07a98752
SiriusSeptimus <tombo@LAPTOP-KLULIUKI>
2025-11-24 13:20:40 -0500

    Finsihed adding the implementation of adding users. Need to get API working again

--- Student Tracker Blazor/Admin/ManageUsers.cs
+++ Student Tracker Blazor/Admin/ManageUsers.cs
@@ -4,6 +4,7 @@ using System.Text;
 using Microsoft.AspNetCore.Components;
 using Microsoft.EntityFrameworkCore.Metadata.Conventions;
 using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Student_Tracker_Blazor.Components;
 
 namespace Student_Tracker_Blazor
@@ -30,8 +31,10 @@ namespace Student_Tracker_Blazor
                     navMan.NavigateTo("/adminaddadmin");
                     break;
                 case "student":
+                    navMan.NavigateTo("/adminaddstudent");
                     break;
                 case "professor":
+                    navMan.NavigateTo("/adminaddprofessor");
                     break;
             }
         }
@@ -52,6 +55,43 @@ namespace Student_Tracker_Blazor
 
            
             APIServices.CreateAdminAsync(user, andmin);
+        }
+
+        public void AddProfessor(string emailAddr, string password, string firstName, string lastName)
+        {
+            UserJson user = new UserJson();
+
+            user.emailaddr = emailAddr;
+            user.password = password;
+            user.firstName = firstName;
+            user.lastName = lastName;
+            user.createdAt = DateTime.Now.ToString();
+            user.updatedAt = DateTime.Now.ToString();
+
+
+            TeacherJson professor = new TeacherJson();
+ 
+
+            APIServices.CreateTeacherAsync(user, professor);
+        }
+
+        public void AddStudent(string emailAddr, string password, string firstName, string lastName, string enumber, int professorId)
+        {
+            UserJson user = new UserJson();
+
+            user.emailaddr = emailAddr;
+            user.password = password;
+            user.firstName = firstName;
+            user.lastName = lastName;
+            user.createdAt = DateTime.Now.ToString();
+            user.updatedAt = DateTime.Now.ToString();
+
+
+            StudentJson student = new StudentJson();
+            student.enumber = enumber;
+            student.professorId = professorId;
+
+            APIServices.CreateStudentAsync(user, student);
         }
     }
 }
